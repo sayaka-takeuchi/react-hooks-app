370de7528712bf17b0343d8c4a354b2a1c8d7a84
sayaka-takeuchi <ars0915.cute0910@gmail.com>
2021-05-29 14:58:31 +0900

    imprement component using multiple states

--- src/App.js
+++ src/App.js
@@ -1,49 +1,30 @@
 import React, { useState } from 'react';
 
-const App = () => {
-  const [count, setCount] = useState(0);
+const App = (props) => {
+  const [name, setName] = useState(props.name);
+  const [price, setPrice] = useState(props.price);
 
-  const increment = () => setCount(count + 1);
-  const decrement = () => setCount(count - 1);
+  const reset = () => {
+    setPrice(props.price);
+    setName(props.name);
+  };
 
-  const increment2 = () => setCount((previousCount) => previousCount + 1);
-  const decrement2 = () => setCount((previousCount) => previousCount - 1);
-  const reset = () => setCount(0);
-  const double = () => setCount(count * 2);
-  console.log(count);
-
-  // const divideByThree = () =>
-  //   setCount((previousCount) => {
-  //     if (previousCount % 3 === 0) {
-  //       return previousCount / 3;
-  //     } else {
-  //       return previousCount;
-  //     }
-  //   });
-
-  // 三項演算子で短く記述
-  const divideByThree = () =>
-    setCount((previousCount) =>
-      previousCount % 3 === 0 ? previousCount / 3 : previousCount
-    );
   return (
     <>
-      <div>{count}</div>
-      <div>
-        <button onClick={increment}>+1</button>
-        <button onClick={decrement}>-1</button>
-      </div>
-      <div>
-        <button onClick={increment2}>+1</button>
-        <button onClick={decrement2}>-1</button>
-      </div>
-      <div>
-        <button onClick={reset}>Reset</button>
-        <button onClick={double}>x2</button>
-        <button onClick={divideByThree}>3の倍数の時だけ３で割る</button>
-      </div>
+      <p>
+        現在の{name}は{price}円です。
+      </p>
+      <button onClick={() => setPrice(price + 1)}>+1</button>
+      <button onClick={() => setPrice(price - 1)}>-1</button>
+      <button onClick={reset}>Reset</button>
+      <input value={name} onChange={(e) => setName(e.target.value)} />
     </>
   );
+};
+
+App.defaultProps = {
+  name: '',
+  price: 1000,
 };
 
 export default App;
